2726afdbb598c5254c9e6164af5d700da1672024
floriangeron <floriangeron98@gmail.com>
2024-12-22 16:51:14 +0100

    More facts

--- florian-geron/app/routes/funfacts.tsx
+++ florian-geron/app/routes/funfacts.tsx
@@ -10,24 +10,26 @@ export const meta: MetaFunction = () => {
 };
 
 const funfacts = [
-    "The Vatican classifies capybara as fish.",
+    "The Vatican classifies capybara as fish. This allows Latin Americans to eat the animal during Lent.",
     "Antartica has the lowest infant mortality rate of any continent.",
     "There are only two double landlocked countries, i.e. countries who only border landlocked countries. They are Liechtenstein and Uzbekistan.",
     "The Caspian Sea is a lake, not a sea.",
     "Owls' eyes have a conical shape. This means their eyes cannot rotate in their sockets, which is why their neck can turn more than 360 degrees.",
     "Flamenco is a renowned Spanish style of dance and music. The Spanish word for Flemish happens to be Flamenco too. It is thought that the music genre was named after the guitar which is used to make the music. This type of guitar was introduced to Spain by Emperor Charles V and his court. Charles V was Flemish.",
-    "Chess is known as 'schaak' in Dutch and 'Schach' in German. These names all derive from the  Persian word for king, 'Shah'. The word 'check mate' comes from Persian too, meaning 'the king is dead'.",
+    "Chess is known as 'schaak' in Dutch and 'Schach' in German. These names all derive from the  Persian word for king, 'Shah'. The word 'checkmate' comes from Persian too, meaning 'the king is dead'.",
     "The canary islands are not named after the birds. Rather, they are named after the dogs straying the island: The Greek word for dog is 'κύων', the adjective being 'κυνικός'. This is also where we get the word 'canine' from. The birds were then named after the islands.",
     "The original cynic philosopher, Diogenes, is said to have had an interaction with Alexander the Great. Alexander, who admired the philosopher, told him to make a wish - If it was within his power, Alexander would do his best to fulfil the wish. Diogenes asked him to take a step to the left: Alexander was blocking out the sun.",
     "The original cynic philosopher, Diogenes, lived his life as frugally as possible. He possessed three things: A barrel to sleep in, a plate to eat from, and a cup to drink from. When he saw a begger drink from a fountain by molding his hands into a cup, he threw away his cup, happy to be able to throw away one more of his possessions. Diogenes liked to compare his lifestyle to that of a dog. The Greek word for canine is 'κυνικός'. This is where we get the word 'cynical' from, Greek for 'like a dog'.",
     "The river Rhine and diarrhea share an etymological root. The Greek word 'ῥειν' means 'to flow'. Diarrhea literally means 'to flow through'.",
-    "'Πάντα ῥεῖ καὶ οὐδὲν μένει' means 'everything flows and nothing stays'.",
+    "'Πάντα ῥεῖ καὶ οὐδὲν μένει' means 'everything flows and nothing stays [still]'.",
     "Cyprus has more cats than humans.",
     "Australia has more kangaroos than humans.",
     "Andalusia, a region is Spain, has an interesting etymology. After the fall of the Western Roman Empire, different Germanic tribes conquered different parts of the Empire. The Vandals conquered Iberia. Some centuries later, the Moors conquered it in turn. They called it 'the land of the Vandals'. However, Arabic doesn't have the V-sound, so it is pronounced as Al-Andalus, which became Andalusia in modern Spanish.",
     "Having a space port closer to the equator reduces the fuel needed to launch a spacecraft into orbit. (Earth's spin provides a kinetic boost.) This is why countries build their space ports as close to the equator as possible: The US in Florida, the Soviet Union in Kazachstan, and the EU in French Guyana.",
     "The Arctic is named after bears: It comes from the Greek word 'ἄρκτος', adjective 'ἀρκτικός'. The region isn't names after polar bears though. It is named after the constellations, the Little and the Great Bear, which are always situated towards the Northern half of the sky. The Latin names for these constallations are Ursa Minor and Ursa Major.",
-    "Normandy, the region in France, was given to the French king to a group of vikings. This group has nearly sacked Paris. Despite this aggression, the king was so impressed by their nack for warfare, that he proposed to give them a piece of land along the French coast. In return, the vikings would protect France from any other seaborn invaders. This region came to be known as the region of the Northmen, i.e. Normandy."
+    "Normandy, the region in France, was given to a group of vikings by the French king. The vikings had nearly sacked Paris. The king was so impressed by their nack for warfare, that - despite the aggression - he gave them a piece of land along the French coast. In return, the vikings would protect France from any other seaborn invaders. This region came to be known as the region of the Northmen, i.e. Normandy.",
+    "The croissant is a crescant-shaped pastry. In contemporary culture, it is associated with the French cuisine, but it orginated in Austria, whose inhabitants called it 'kipferl'. This is why the French refer to certain baked goods as 'Viennoiserie'. It is said the Viennese came up with the design after successfully repelling the Ottoman siege of Vienna of 1683: The flags of the fleeing Ottoman army, which contain a crescent moon, inspired Viennese bakers. In reality, the kipferl predates this battle.",
+    "The mannequin is a lifelike doll used to try out clothes. Due to it French sound, it is associated with French haute-couture. However, it derives from the Flemish word 'manneken', meaning 'little man'. The mannequin gained popularity in the Flemish textile industry. The French, who either ruled or heavily influenced Flanders for most of its history, borrowed the word and changed its spelling and pronunciation. Most of the world now uses the French word, which has even found its way back into the Flemish vocabulary, displacing the original Flemish spelling and pronunciation."
 
 ]
 
