a2466fd245ebf4d665431e62d8992e342caeef9b
floriangeron <floriangeron98@gmail.com>
2024-12-30 21:13:02 +0100

    Updated fun fact

--- florian-geron/app/routes/funfacts.tsx
+++ florian-geron/app/routes/funfacts.tsx
@@ -86,6 +86,8 @@ const funfacts = [
     "In old Dutch, the word for 'apple' was a generic term used to refer to any kind of fruit. This is why, in Dutch, 'an orange' is 'een appelsien', meaning 'apple from China': The fruit originated in the East, which the Dutch associated with China. Similarly, 'a potato' is 'een aardappel', meaning 'ground apple'.",
     "The colour orange is named after the fruit. This word only entered the English language around the 15th century. Before this time, English speakers used the word 'yellow-red', for orange.\n\nThe fruit gets its name from the Sanskrit 'nāraṅga' which refers to the orange tree. The word found its way from Sanskrit to Persian to Arabic to French, and then finally to English.",
     "In the 20th century, an influential Arab family, the House of Saud, unified most of the Arabian peninsula. They named this newly unified country 'Saudi Arabia', which would be a bit like the UK being called 'Windsorland' or the Netherlands being called 'Oranjeland'.",
+    "Andorra is the largest country without an airport",
+    // 70
 
 ]
 
