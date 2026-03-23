93a48459c63fe263c556cc9d0570282b97d88419
fgeron <fgeron@deloitte.com>
2026-03-23 14:19:18 +0100

    Change Cambodia index description

--- florian-geron/app/routes/travelblog._index.tsx
+++ florian-geron/app/routes/travelblog._index.tsx
@@ -27,7 +27,7 @@ export default function TravelBlog() {
             date: "December 2025",
             excerpt: "A journey through time and history...",
             content:
-                "Join me on a journey through the ancient temples around Angkor Wat, the world's largest collection of ancient temples, hidden in the jungles of Cambodia.",
+                "Join me on a journey through the ancient temples around Angkor Wat, the world's largest collection of ancient temples, hidden in the jungles of Cambodia. After, we head to the Koh Rong islands for some well-deserved beach time.",
             imageUrl: "/images/travel/Angkor.jpeg",
         },
         {
