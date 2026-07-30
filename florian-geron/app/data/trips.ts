cac316ff10626955dad2c0288655a0f1fbd85a34
floriangeron <floriangeron98@gmail.com>
2026-07-30 19:19:55 +0200

    Quarantine trips metadata

---
+++ florian-geron/app/data/trips.ts
@@ -0,0 +1,85 @@
+export const trips = [
+    {
+        id: "southernvietnam",
+        title: "The Highlights of Southern Vietnam",
+        date: "December 2025",
+        excerpt: "Exploring the main sights of Vietnam's southern region...",
+        content:
+            "Join me on our exploration of Ho Chi Minh City and some of its surrounding natural beauty. With only one week to spend here, we explored the city itself and the impressive Mekong Delta to its South.",
+        imageUrl: "/images/travel/vietnam/hochiminh.JPG",
+        coordinates: [10.77490095095088, 106.70232940425227],
+    },
+    {
+        id: "angkor",
+        title: "Exploring the Ancient Wonders of Angkor Wat",
+        date: "December 2025",
+        excerpt: "A journey through time and history...",
+        content:
+            "Join me on a journey through the ancient temples around Angkor Wat, the world's largest collection of ancient temples, hidden in the jungles of Cambodia. After, we head to the Koh Rong islands for some well-deserved beach time.",
+        imageUrl: "/images/travel/cambodia/angkor/Angkor.jpeg",
+        coordinates: [13.412458850733435, 103.86843408922304],
+    },
+    {
+        id: "laos",
+        title: "Sunsets and Adventure in Laos",
+        date: "November 2025",
+        excerpt: "Climbing many a mountain for a sunset...",
+        content:
+            "Join me on our one-week journey through Laos, exploring some of the beautiful landscapes and towns of this hidden gem in Southeast Asia.",
+        imageUrl: "/images/travel/laos/Laos.jpg",
+    },
+    {
+        id: "thailand",
+        title: "Thailand Speedrun",
+        date: "November 2025",
+        excerpt: "How much of Thailand can one see in two weeks?",
+        content:
+            "Join me to find out how much of Thailand we can cover in two weeks. We'll visit the mountains of the North, the beaches of the South, and the capital of Bangkok.",
+        imageUrl: "/images/travel/thailand/Thailand.JPG",
+    },
+    {
+        id: "vienna",
+        title: "City Trip to Vienna",
+        date: "July 2025",
+        excerpt: "City trip to Vienna",
+        content:
+            "Join me on a city trip to Vienna, a capital fit for an Empire, as well as a contemporary city of music, art and architecture.",
+        imageUrl: "/images/travel/Vienna.jpg",
+    },
+    {
+        id: "cinqueterre",
+        title: "Discovering Cinque Terre",
+        date: "August 2024",
+        excerpt: "Ligurian coast in blistering summer heat",
+        content:
+            "Join me on a quick tour through the Ligurian coast's most picturesque cities, the Cinque Terre.",
+        imageUrl: "/images/travel/CinqueTerre.JPG",
+    },
+    {
+        id: "malta",
+        title: "Nature and Culture on Malta",
+        date: "May 2024",
+        excerpt: "Exploring Malta",
+        content:
+            "Join me in exploring Malta, the EU's smallest Member State located in the middle of the Mediterranean.",
+        imageUrl: "/images/travel/Malta.jpg",
+    },
+    {
+        id: "albania",
+        title: "Albania - Mountain climbing and beach chilling",
+        date: "September 2023",
+        excerpt: "Albania",
+        content:
+            "Join me on our one-week journey through Albania, where we tried to cover as much of this country as we could.",
+        imageUrl: "/images/travel/Albania.JPG",
+    },
+    {
+        id: "fiji",
+        title: "Exploring Fiji's tropical islands",
+        date: "June 2023",
+        excerpt: "Fiji",
+        content:
+            "Join me on a five-day tour to some of Fiji's tropical little islands.",
+        imageUrl: "/images/travel/Fiji.jpg",
+    },
+];
